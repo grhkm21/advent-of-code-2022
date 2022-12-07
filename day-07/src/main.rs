42e2c7b2c76403d92392db3ff8efb3fa5bba50dc
Gareth Ma <grhkm21@gmail.com>
2022-12-07 13:27:58 +0000

    Refactored day-07 slightly with blanket implementations.

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -4,29 +4,35 @@ use std::fmt::Debug;
 use std::ops::Add;
 use std::rc::Rc;
 
+const ITER_ERR: &str = "err: iterator is empty";
 const PARSE_ERR: &str = "err: can't parse int";
 const SPLIT_ERR: &str = "err: can't split string";
 
-struct Solver<'a> {
-    content: &'a str,
+struct Solver {
+    content: String,
 }
 
 trait Solvable {
     fn solve(self) -> (usize, usize);
 }
 
+// Blanket implementation
+trait NodeValTrait<T>: Add<Output = T> + Default + Copy + Debug {}
+
+impl<T> NodeValTrait<T> for T where T: Add<Output = T> + Default + Copy + Debug {}
+
 #[derive(Debug)]
-struct TreeNode<T: Add<Output = T> + Default + Copy + Debug> {
+struct TreeNode<T: NodeValTrait<T>> {
     val: Option<T>,
     name: String,
     children: Vec<Rc<RefCell<TreeNode<T>>>>,
 }
 
-impl<T: Add<Output = T> + Default + Copy + Debug> TreeNode<T> {
-    fn new(s: String) -> TreeNode<T> {
+impl<T: NodeValTrait<T>> TreeNode<T> {
+    fn new(s: impl ToString) -> TreeNode<T> {
         TreeNode {
             val: None,
-            name: s,
+            name: s.to_string(),
             children: Vec::new(),
         }
     }
@@ -39,7 +45,8 @@ impl<T: Add<Output = T> + Default + Copy + Debug> TreeNode<T> {
         self.children.push(Rc::new(RefCell::new(node)));
     }
 
-    fn insert_and_find(&mut self, name: String) -> Rc<RefCell<TreeNode<T>>> {
+    fn insert_and_find(&mut self, name: impl ToString) -> Rc<RefCell<TreeNode<T>>> {
+        let name = name.to_string();
         for child in &self.children {
             if child.borrow().name == name {
                 return Rc::clone(child);
@@ -61,8 +68,6 @@ impl<T: Add<Output = T> + Default + Copy + Debug> TreeNode<T> {
         res
     }
 
-    // I can't get it to return the TreeNode itself
-    // Returning the .name is best I can do
     fn dfs<F, T2>(&self, f: F) -> Vec<T2>
     where
         F: Fn(&TreeNode<T>) -> T2,
@@ -70,107 +75,82 @@ impl<T: Add<Output = T> + Default + Copy + Debug> TreeNode<T> {
         let mut res = Vec::new();
         res.push(f(self));
         for child in &self.children {
-            let child = child.borrow_mut();
-            for val in child.dfs(&f as &dyn Fn(&TreeNode<T>) -> T2) {
-                res.push(val);
-            }
+            // workaround for infinite recursion
+            let f = &f as &dyn Fn(&TreeNode<T>) -> T2;
+            res.extend(child.borrow_mut().dfs(f));
         }
         res
     }
 }
 
-impl<T: Add<Output = T> + Default + Copy + Debug> Default for TreeNode<T> {
-    fn default() -> Self {
-        Self::new("".to_string())
-    }
-}
-
-impl<'a> Solvable for Solver<'a> {
+impl Solvable for Solver {
     fn solve(self) -> (usize, usize) {
-        let root: Rc<RefCell<TreeNode<usize>>> = Default::default();
-        let mut cur_len = 0;
+        let root: Rc<RefCell<TreeNode<usize>>> = Rc::new(RefCell::new(TreeNode::new("")));
         let mut cur_path = Vec::new();
         cur_path.push(Rc::clone(&root));
 
+        // each $ represents new command input / output group
         for cmds in self.content.split("$ ") {
-            let cur = &cur_path[cur_len];
+            let cur = &cur_path[cur_path.len() - 1];
             let lines: Vec<&str> = cmds.trim().lines().collect();
             if lines.is_empty() {
                 continue;
             }
 
+            // process each group of commands
             let cmd: Vec<&str> = lines[0].split(" ").collect();
             if cmd[0] == "ls" {
                 for &line in &lines[1..] {
                     let (prefix, dir) = line.split_once(" ").expect(SPLIT_ERR);
-                    let dir = dir.to_string();
-                    if prefix == "dir" {
-                        cur.borrow_mut().insert_and_find(dir);
-                    } else {
-                        let size: usize = prefix.parse().expect(PARSE_ERR);
-                        let child = cur.borrow_mut().insert_and_find(dir);
-                        child.borrow_mut().val(size);
+                    let child = cur.borrow_mut().insert_and_find(dir);
+                    match prefix {
+                        "dir" => {
+                            cur.borrow_mut().insert_and_find(dir);
+                        }
+                        _ => {
+                            let prefix = prefix.parse().expect(PARSE_ERR);
+                            child.borrow_mut().val(prefix);
+                        }
                     }
                 }
             } else {
-                match cmd[..] {
-                    ["cd", ".."] => {
-                        cur_len -= 1;
+                match cmd[1] {
+                    ".." => {
                         cur_path.pop();
                     }
-                    ["cd", dir] => {
-                        let child = cur.borrow_mut().insert_and_find(dir.to_string());
-                        cur_len += 1;
+                    dir => {
+                        let child = cur.borrow_mut().insert_and_find(dir);
                         cur_path.push(child);
                     }
-                    _ => unreachable!(),
                 }
             }
         }
 
-        let func = |node: &TreeNode<usize>| {
-            (
-                if let Some(_) = node.val { true } else { false },
-                node.sum(),
-            )
-        };
-        let root = root.borrow_mut();
-        let vals = root.dfs(func);
+        let func = |node: &TreeNode<usize>| (node.val.is_some(), node.sum());
+        let binding = root.borrow_mut().dfs(func);
+        let vals = binding
+            .iter()
+            .filter(|(is_file, _)| !is_file)
+            .map(|(_, sum)| sum);
 
         // part 1
-        let mut part1 = 0;
-        for (is_file, sum) in vals.clone() {
-            // println!(
-            //     "[{}] name = {name:?}, sum = {sum:?}",
-            //     if is_file { "-" } else { "+" }
-            // );
-            if is_file {
-                continue;
-            }
-            if sum <= 100000 {
-                part1 += sum;
-            }
-        }
+        let part1 = vals.clone().filter(|&sum| *sum <= 100000).sum();
 
         // part 2
-        let freeup = root.sum() - 40000000;
-        let mut part2 = usize::MAX;
-        for (is_file, sum) in vals.clone() {
-            if is_file {
-                continue;
-            }
-            if sum >= freeup && sum < part2 {
-                part2 = sum;
-            }
-        }
+        let freeup = root.borrow_mut().sum() - 40000000;
+        let part2 = vals
+            .clone()
+            .filter(|&sum| *sum >= freeup)
+            .min()
+            .expect(ITER_ERR);
 
-        (part1, part2)
+        (part1, *part2)
     }
 }
 
 fn main() {
     let solver = Solver {
-        content: include_str!("../input"),
+        content: include_str!("../input").to_string(),
     };
     let (part1, part2) = solver.solve();
     println!("Part 1: {part1:?}");
